9f6ea68cff73629597277499f6e5a71dd379d777
elpablotoC <110862073+elpablotoC@users.noreply.github.com>
2024-10-08 20:01:27 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import discord
+from settings import settings
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send(f'Hola, soy un bot {client.user}!')
+    elif  message.content.startswith('$heh'):
+        if len(message.content) > 4:
+            count_heh = int(message.content[4:])
+        else:
+            count_heh = 5
+        await message.channel.send("he" * count_heh)
+    elif message.content.startswith  ("$userinfo"):
+        user_id = message.author.id
+        user = await client.fetch_user(user_id)
+        await  message.channel.send(f'usuario: {user.name}, id: {user.id}' )
+
+
+
+client.run(settings["TOKEN"])
+
+import random
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "HEADS"
+    else:
+        return "TAILS"
